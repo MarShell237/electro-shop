6f25223c742614e016000f28ca3a84442b87d24c
MarShell237 <marceldjiofack@outlook.com>
2025-02-04 04:33:16 +0100

    vercel.json 2

--- api/index.php
+++ api/index.php
@@ -1,12 +1,6 @@
 <?php
-require __DIR__.'/../vendor/autoload.php';
-
-$app = require __DIR__.'/../bootstrap/app.php';
-
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(Illuminate\Http\Request::capture());
-
-$response->send();
-
-$kernel->terminate(Illuminate\Http\Request::capture(), $response);
+/**
+ * Here is the serverless function entry
+ * for deployment with Vercel.
+ */
+require __DIR__.'/../public/index.php';
